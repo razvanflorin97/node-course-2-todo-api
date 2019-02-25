a01b10a70359881502c36edc9a46e2af102e02c9
Nicolae Razvan Florin <razvannicolae10@gmail.com>
2019-02-25 14:41:00 +0200

    Update age and name for the user with id 1

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,15 +9,29 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
-  db.collection('Users').findOneAndUpdate({_id: new ObjectID('5c73abb00a38683050adc057')}, {
+  // db.collection('Users').findOneAndUpdate({_id: new ObjectID('5c73abb00a38683050adc057')}, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({_id: 1}, {
     $set: {
-      completed: true
+      name: 'Nicolae Razvan Florin'
+    },
+    $inc: {
+      age: -1
     }
   }, {
     returnOriginal: false
-  }).then((result) => {
+  })
+  .then((result) => {
     console.log(result);
-  });
+  })
 
   //client.close();'
 });
