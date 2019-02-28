e54eddb0fb17c59b519e5471d2537abdb164792b
Nicolae Razvan Florin <razvannicolae10@gmail.com>
2019-02-28 16:14:23 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 //Local
 const {mongoose} = require('./db/db');
@@ -65,6 +66,33 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id', (req, res) =>{
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  console.log(body);
+   if (_.isBoolean(body.completed) && body.completed) {
+  // if (!!body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  todoModel.findByIdAndUpdate(id, {$set : body}, {new: true})
+  .then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  })
+  .catch((err) => {
+    res.status(400).send();
+  })
+})
 
 
 
